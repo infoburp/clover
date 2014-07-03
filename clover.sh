569e9db7df5c30cd2cacca990d95f77f47c97bed
Graeme Wolfendale <gwolfendale@gmail.com>
2014-07-03 22:04:27 +0100

    Update clover.sh

--- clover.sh
+++ clover.sh
@@ -2,6 +2,5 @@
 apt-key adv --fetch-keys http://cloverproject.org/apt/clover.gpg.key
 echo "deb http://cloverproject.org/apt testing main" >> /etc/apt/sources.list 
 apt-get update
-apt-get install i3 feh git
+apt-get install i3 feh git xorg
 git clone http://github.com/infoburp/clover
-feh --bg-scale ~/clover/wall.png
